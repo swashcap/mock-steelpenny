ac0ff8586a0a0d73d1acc4b974f206381b1a30b6
Cory Reed <swashcap@gmail.com>
2017-05-22 23:14:25 -0700

    Scaffold out mock server with hapi.

---
+++ api.js
@@ -0,0 +1,149 @@
+'use strict'
+
+const atob = require('atob')
+const coinstacConfig = require('/coins/config/dbmap.json').coinstac
+
+const getUserResponse = (username, coinstac = false) => {
+  const response = {
+    algorithm: 'sha256',
+    expireTime: Date.now() + 24 * 60 * 60,
+    id: '94af21e3-a581-4b1a-a8b0-180d81e3ad85',
+    issueTime: Date.now(),
+    key: 'b09945f7-b768-47ee-b003-59e0a1b5c76a',
+    studyRoles: {},
+    user: {
+      acctExpDate: '2017-06-09T06:00:00.000Z',
+      activeFlag: 'Y',
+      dateAdded: '2017-06-09T06:00:00.000Z',
+      email: 'nidev@mrn.org',
+      emailUnsubscribed: false,
+      isSiteAdmin: 'Y',
+      label: username,
+      passwordExpDate: '2016-12-07T07:00:00.000Z',
+      passwordResetExpiration: null, // TODO: populate
+      passwordResetKey: null,
+      passwordResetSessionId: null, // TODO: populate
+      siteId: '7',
+      username
+    },
+    username
+  }
+
+  if (coinstac) {
+    response.coinstac = coinstacConfig
+  }
+
+  return response
+}
+
+const formatResponse = response => ({
+  data: Array.isArray(response) ? response : [response],
+  error: null,
+  stats: {}
+})
+
+module.exports.register = (server, options, next) => {
+  server.route({
+    handler (request, reply) {
+      return reply('Not implemented').code(501)
+    },
+    method: 'GET',
+    path: '/'
+  })
+
+  server.route({
+    handler (request, reply) {
+      return reply('Not implemented').code(501)
+    },
+    method: 'GET',
+    path: '/auth/cookies/{id}'
+  })
+
+  server.route({
+    handler (request, reply) {
+      return reply('Not implemented').code(501)
+    },
+    method: 'OPTIONS',
+    path: '/auth/keys/{id}'
+  })
+
+  server.route({
+    handler (request, reply) {
+      const { payload: { coinstac, username } } = request
+
+      return reply(
+        formatResponse(getUserResponse(atob(username), coinstac))
+      )
+        .code(201)
+    },
+    method: 'POST',
+    path: '/auth/keys'
+  })
+
+  server.route({
+    handler (request, reply) {
+      return reply('Not implemented').code(501)
+    },
+    method: 'DELETE',
+    path: '/auth/keys/{id}'
+  })
+
+  server.route({
+    handler (request, reply) {
+      return reply('Not implemented').code(501)
+    },
+    method: 'POST',
+    path: '/auth/reset-request'
+  })
+
+  server.route({
+    handler (request, reply) {
+      return reply('Not implemented').code(501)
+    },
+    method: 'POST',
+    path: '/auth/reset'
+  })
+
+  server.route({
+    handler (request, reply) {
+      reply(formatResponse({
+        description: 'Test site',
+        label: 'testsite',
+        siteConfig: {
+          administratorContact: 'test@test.world',
+          expirationEmailMessage: 'Sample expiration message'
+        },
+        siteId: 100
+      }))
+    },
+    method: 'GET',
+    path: '/sites'
+  })
+
+  server.route({
+    handler (request, reply) {
+      return reply('Not implemented').code(501)
+    },
+    method: 'GET',
+    path: '/users/check'
+  })
+
+  server.route({
+    handler (request, reply) {
+      const { payload: { username } } = request
+
+      return reply(
+        formatResponse(getUserResponse(atob(username)).user)
+      )
+        .code(201)
+    },
+    method: 'POST',
+    path: '/users'
+  })
+
+  next()
+}
+
+module.exports.register.attributes = {
+  name: 'api'
+}
--- index.js
+++ index.js
@@ -1,122 +1,51 @@
-'use strict';
+'use strict'
 
-const http = require('http');
+const good = require('good')
+const hapi = require('hapi')
+const pify = require('pify')
 
-const PORT = 8800;
+const api = require('./api.js')
 
-function errorHandler(error) {
-  console.error(error);
-  process.exit(1);
-}
-
-/**
- * @param {Object} request
- * @param {string} request.body
- * @param {string} request.url
- * @param {string} request.method
- * @returns {Object}
- */
-function getApiResponse({ body, method, url }) {
-  try {
-    const parsed = JSON.parse(body);
-
-    /**
-     * Manual atob so there's no deps.
-     *
-     * {@link https://github.com/node-browser-compat/atob}
-     */
-    const username = new Buffer(parsed.username, 'base64').toString('binary');
-
-    if (!username) {
-      throw new Error('username required');
-    }
-
-    const response = {
-      body: {
-        data: [{
-          username,
-          user: {
-            username,
-            label: username,
-            activeFlag: 'Y',
-            acctExpDate: '2017-06-09T06:00:00.000Z',
-            passwordExpDate: '2016-12-07T07:00:00.000Z',
-            siteId: '7',
-            isSiteAdmin: 'Y',
-            email: 'nidev@mrn.org',
-            emailUnsubscribed: false,
-          },
-          id: '94af21e3-a581-4b1a-a8b0-180d81e3ad85',
-          key: 'b09945f7-b768-47ee-b003-59e0a1b5c76a',
-          algorithm: 'sha256',
-          issueTime: Date.now(),
-          expireTime: Date.now() + 24 * 60 * 60,
-          studyRoles: {},
-        }],
-        error: null,
-        stats: {}
-      },
-      statusCode: 201,
-    };
-
-    if (parsed.coinstac) {
-      response.body.data[0].coinstac = require('/coins/config/dbmap.json').coinstac;
-    }
-
-    return response;
-  } catch (error) {
-    return {
-      body: {
-        data: [],
-        error: `Couldn't parse request: ${error.toString()}`,
-        stats: {},
-      },
-      statusCode: 400,
-    };
+const server = new hapi.Server({
+  debug: {
+    request: '*'
   }
+})
 
-}
+const register = pify(server.register.bind(server))
 
-function onRequest(request, response) {
-  const { method, url } = request;
-  let body = '';
+server.connection({
+  host: 'localhost',
+  port: 8800
+})
 
-  console.log('Request:', method, url);
-
-  request.on('readable', () => {
-    const chunk = request.read();
-
-    if (chunk === null) {
-      const {
-        body: responseBody,
-        statusCode,
-      } = getApiResponse({ body, method, url });
-
-      response.writeHead(statusCode, { 'Content-Type': 'application/json' });
-      response.end(JSON.stringify(responseBody));
-    } else {
-      body += chunk.toString();
+register({
+  register: good,
+  options: {
+    reporters: {
+      myConsoleReporter: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{ log: '*', response: '*' }]
+      }, {
+        module: 'good-console'
+      }, 'stdout']
     }
-  });
-
-  request.on('error', error => {
-    errorHandler(error);
-  });
-}
-
-process.on('uncaughtException', errorHandler);
-process.on('unhandledRejection', (reason, p) => {
-  console.error("Unhandled Rejection at: Promise ", p, " reason: ", reason);
-});
-
-const server = http
-  .createServer(onRequest)
-  .listen(PORT, error => {
-    if (error) {
-      errorHandler(error);
-    } else {
-      console.log(`Server listening on ${PORT}`);
+  }
+})
+  .then(() => register(
+    { register: api },
+    {
+      routes: {
+        prefix: '/api/v1.3.0'
+      }
     }
-  });
+  ))
+  .then(() => pify(server.start.bind(server)()))
+  .then(() => console.log(`Server running at: ${server.info.uri}`))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
 
-module.exports = server;
+module.exports = server
