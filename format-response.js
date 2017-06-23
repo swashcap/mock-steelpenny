39bb2a288ece2d99017b615384b6dd948710ecf9
Cory Reed <swashcap@gmail.com>
2017-06-23 15:18:38 -0700

    Ensure error reponses match steelpenny behavior.

--- format-response.js
+++ format-response.js
@@ -1,14 +1,32 @@
+const url = require('url')
 
 module.exports.register = (server, options, next) => {
   server.ext('onPreResponse', (request, reply) => {
     if (request.response.isBoom) {
       return reply({
-        data: null,
+        data: [],
         error: Object.assign({}, request.response.output.payload, {
           debugData: request.response.data
         }),
         stats: {}
-      }).code(request.response.output.statusCode)
+      })
+        /**
+         * steelpenny's nginx reverse proxy still sends CORS headers with
+         * responses on invalid requests. Match this behavior.
+         */
+        .header('Access-Control-Allow-Credentials', 'true')
+        .header(
+          'Access-Control-Allow-Origin',
+          url.format(Object.assign(
+            url.parse(request.info.referrer),
+            {
+              pathname: '',
+              query: '',
+              search: ''
+            }
+          ))
+        )
+        .code(request.response.output.statusCode)
     }
 
     return reply({
